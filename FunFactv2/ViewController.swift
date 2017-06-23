2a2450e9b25d479c1300422104514b86de0b3f48
Timothy Hoover <Tim@timothys-imac.local>
2017-06-23 08:30:31 -0700

    Initial Commit

---
+++ FunFactv2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FunFactv2
+//
+//  Created by Timothy Hoover on 6/23/17.
+//  Copyright © 2017 Timothy Hoover. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
